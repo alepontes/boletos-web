5737613cee37d15158ccf3a550fea8a159016c9b
alepontes <alexandrelopespontes@gmail.com>
2024-10-20 20:07:11 -0300

    Adicionar chamada a API

--- src/api/dashboard.ts
+++ src/api/dashboard.ts
@@ -1,17 +1,14 @@
+import axios from "axios";
 import {Dashboard} from "../interfaces/Dashboard.ts";
 
 /**
- * Recupera os filtros
+ * Recupera os dados de Dashboard
  */
 export const getDashboard = async (): Promise<Dashboard> => {
-    return Promise.resolve({
-        energy: {
-            total: 100,
-            compensated: 30,
-        },
-        financial: {
-            total: 100,
-            saving: 50,
-        },
+    return new Promise((resolve, reject) => {
+        axios
+            .get("http://127.0.0.1:8080/dashboard")
+            .then((response) => resolve(response.data as Dashboard))
+            .catch((error: any) => reject(error));
     });
 }
