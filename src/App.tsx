6e0ae8953dff67d0aab14c7d2f2689a1c64f6b4a
alepontes <alexandrelopespontes@gmail.com>
2024-10-20 20:20:08 -0300

    Adicionar navegação simples

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-// import './App.css';
+import {BrowserRouter as Router, Route, Link, Routes, Navigate} from 'react-router-dom';
 import Dashboard from './pages/dashboard';
-// import Invoices from './pages/invoices';
-
+import Invoices from './pages/invoices';
+import './App.css';
 
 function App() {
     return (
-        <>
-            <Dashboard/>
-            {/*<Invoices/>*/}
-        </>
+        <Router>
+            <div>
+                <header className="app-header">
+                    <nav className="nav-bar">
+                        <Link to="/dashboard" className="nav-button">Dashboard</Link>
+                        <Link to="/invoices" className="nav-button">Faturas</Link>
+                    </nav>
+                </header>
+                <main className="app-content">
+                    <Routes>
+                        <Route path="*" element={<Navigate to="/invoices" />} />
+                        <Route path="/dashboard" element={<Dashboard />} />
+                        <Route path="/invoices" element={<Invoices />} />
+                    </Routes>
+                </main>
+            </div>
+        </Router>
     );
 };
 
